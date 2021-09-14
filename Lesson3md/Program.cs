ff7db685abe09f51386173ce63c4298b40220385
Mihails Smirnovs <omnitacc@gmail.com>
2021-09-14 21:57:47 +0300

    Added fist two tasks (not  combined in main class)

--- Lesson3md/Program.cs
+++ Lesson3md/Program.cs
@@ -6,7 +6,17 @@ namespace Lesson3md
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //some code
+            var phone = new Telefons(70.5, 30.2, 5.4);
+
+            phone.Marka = "Samsung";
+            phone.Modelis = "Galaxy S20+";
+            phone.Height = 71.1;
+            phone.Width = 25.5;
+            phone.Depth = 5.5;
+
+            Console.WriteLine(phone.GetNaming()+" and "+ phone.GetSize());
+
         }
     }
 }
---
+++ Lesson3md/Telefons.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3md
+{
+    class Telefons
+    {
+        public Telefons(double height, double width, double depth)
+        {
+            Height = height;
+            Width = width;
+            Depth = depth;
+        }
+
+        public string Marka { get; set; }
+
+        public string Modelis { get; set; }
+
+        public double Height { get; set; }
+
+        public double Width { get; set; }
+
+        public double Depth { get; set; }
+
+        public string GetSize()
+        {
+            return $"Phone size is {Height} in height, {Width} in width, {Depth} in depth";
+        }
+
+        public string GetNaming()
+        {
+            return $"Phone is made by {Marka} and called {Modelis}";
+        }
+
+        public void MakeACall()
+        {
+            Console.WriteLine("Making some call");
+        }
+
+        public void SendingSMS()
+        {
+            Console.WriteLine("Making some SMS sending");
+        }
+
+    }
+}
