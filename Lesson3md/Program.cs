704fe673e9f057e9049f9b694f27bc5ae4c381b9
Mihails Smirnovs <omnitacc@gmail.com>
2021-09-14 21:27:57 +0300

    Add project files.

---
+++ Lesson3md/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson3md
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
